964a5edc581481cd6a9693589394e924f8bbc0ea
Shady Aziza <shadyaziza@gmail.com>
2023-03-09 20:32:42 +0200

    chore: remove unused provider

--- app/lib/src/localization/providers.dart
+++ app/lib/src/localization/providers.dart
@@ -7,23 +7,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'providers.g.dart';
 
-// @Riverpod(keepAlive: true)
-// AppLocalizations appLocalizations(AppLocalizationsRef ref) {
-//   // 1. initialize from the initial locale
-//   ref.state = lookupAppLocalizations(ui.window.locale);
-//   // 2. create an observer to update the state
-//   final observer = _LocaleObserver((locales) {
-//     ref.state = lookupAppLocalizations(ui.window.locale);
-//   });
-//   // 3. register the observer and dispose it when no longer needed
-//   final binding = WidgetsBinding.instance;
-//   binding.addObserver(observer);
-//   ref.onDispose(() => binding.removeObserver(observer));
-//   // 4. return the state
-
-//   return ref.state;
-// }
-
 class _LocaleObserver extends WidgetsBindingObserver {
   _LocaleObserver(this._didChangeLocales);
   final void Function(List<Locale>? locales) _didChangeLocales;
